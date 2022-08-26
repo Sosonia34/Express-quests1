35dffabb0d37ab7af0c7bf1db592e701cec59329
Sonia34sonia <sonia.vidal@sncf.fr>
2022-08-26 17:42:53 +0200

    express 03

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
@@ -26,3 +28,5 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+app.post("/api/movies", movieHandlers.postMovie);//Express3*/
--- movieHandlers.js
+++ movieHandlers.js
@@ -49,8 +49,26 @@ const getMovieById = (req, res) => {
     res.status(404).send("Not Found");
   }
 };
+//Express3*/
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie, 
 };
